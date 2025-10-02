72d14b7cba5b62364bed9733a9b530cb06f3b476
CaptSinBad <97219716+CaptSinBad@users.noreply.github.com>
2025-10-02 22:41:40 +0100

    Update GuardianNoBuyback.sol

--- contracts/GuardianNoBuyback.sol
+++ contracts/GuardianNoBuyback.sol
@@ -1,19 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract GuardianNoBuyback is Ownable {
     bool public buybacksAllowed = true;
+    
     event BuybacksAllowedSet(bool allowed, address setter);
-
+    
     function setBuybacksAllowed(bool allowed) external onlyOwner {
         buybacksAllowed = allowed;
         emit BuybacksAllowedSet(allowed, msg.sender);
     }
-
+    
     function isBuybackAllowed() external view returns (bool) {
         return buybacksAllowed;
     }
 }
-
